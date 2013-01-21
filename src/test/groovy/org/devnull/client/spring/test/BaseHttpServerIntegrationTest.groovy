45d488d0d810348d8d6bea901372cc81681246d0
Mike Cantrell <mike@devnull.org>
2013-01-20 18:23:25 -0600

    fixed more broken tests from namespace refactoring

--- src/test/groovy/org/devnull/client/spring/test/BaseHttpServerIntegrationTest.groovy
+++ src/test/groovy/org/devnull/client/spring/test/BaseHttpServerIntegrationTest.groovy
@@ -13,6 +13,7 @@ abstract class BaseHttpServerIntegrationTest {
     static void createServer() {
         server = new Server(8081)
         def resources = [
+                "/zuul/settings/prod/app-data-config.properties" :new ClassPathResource("/responses/mock-server-response-des.properties"),
                 "/zuul/settings/dev/test-des-config.properties" :new ClassPathResource("/responses/mock-server-response-des.properties"),
                 "/zuul/settings/prod/test-aes-config.properties" :new ClassPathResource("/responses/mock-server-response-aes.properties"),
                 "/zuul/settings/prod/test-pgp-config.properties" :new ClassPathResource("/responses/mock-server-response-pgp.properties")
